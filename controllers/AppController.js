f7fba5184d3f224458a8b609d04c0742e6a6278f
Tshepiso <tlhongtshepiso2@gmail.com>
2024-12-10 23:54:19 +0200

    API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -5,7 +5,7 @@ class AppController {
   static getStatus(req, res) {
     const redisLive = redisClient.isAlive();
     const dbLive = dbClient.isAlive();
-    res.status(200).json({ redis: redisLive, db: dbLive});
+    res.status(200).json({ redis: redisLive, db: dbLive });
   }
 
   static async getStats(req, res) {
--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
-router.post('/files', FilesController.postUpload)
+router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
 router.put('/files/:id/publish', FilesController.putPublish);
